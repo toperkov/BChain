078d06917f2513c2ac8c32521ac3ef5bcffec623
etnaetna <akrzel00@fesb.hr>
2018-05-26 10:53:46 +0200

    Gui for ReqMaker
    Creating the gui for the transaction maker program.

--- ReqMaker.py
+++ ReqMaker.py
@@ -1,14 +1,50 @@
 import socket
+from tkinter import *
 
-flag = True
-
-while flag:
-    flag = int(input('\n 0 - exit \n 1 - make transation'))
+def SendTransaction(yourID, receiverID, amount):
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.connect(("", 9999))
-    sender = input("Enter your ID")
-    receiver = input("Enter receiver's ID")
-    amount = "Enter coin number"
-    trans = "TRANS" + "," + sender + "," + receiver + "," + amount
+    trans = "TRANS" + "," + yourID + "," + receiverID + "," + amount
     socket.sendall(trans.encode('ascii'))
     socket.close()
+
+
+window = Tk()
+
+yourID = "nestoNestoNesto"
+ 
+window.geometry('500x200')
+ 
+lbl1 = Label(window, text="YourID:")
+ 
+lbl1.grid(column=0, row=0)
+
+lblYourID = Label(window, text=yourID)
+
+lblYourID.grid(column=1, row=0)
+
+lbl2 = Label(window, text="ReceiverID:")
+ 
+lbl2.grid(column=0, row=1)
+ 
+receiverID = Entry(window,width=50)
+ 
+receiverID.grid(column=1, row=1)
+
+lbl3 = Label(window, text="Amount:")
+ 
+lbl3.grid(column=0, row=2)
+ 
+Amount = Entry(window,width=50)
+
+Amount.grid(column=1, row=2)
+Amount.grid(column=1, row=2)
+
+
+ 
+btn = Button(window, text="Send transaction",\
+ command=SendTransaction)
+ 
+btn.grid(column=0, row=3)
+ 
+window.mainloop()
